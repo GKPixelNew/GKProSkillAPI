fb1f0756f2404045cda859a4d58d93e3fff1a2a0
RobotHanzo <RobotHanzo@users.noreply.github.com>
2023-05-27 14:46:27 +0800

    Be reasonable about player walk speed

--- src/main/java/com/sucy/skill/api/player/PlayerData.java
+++ src/main/java/com/sucy/skill/api/player/PlayerData.java
@@ -1659,14 +1659,12 @@ public class PlayerData {
      * @param player the player
      */
     public void updateWalkSpeed(Player player) {
-
-        float level = (float) (this.scaleStat(AttributeManager.MOVE_SPEED, 0.2f, 0D, Double.MAX_VALUE));
+        float level = (float) (this.scaleStat(AttributeManager.MOVE_SPEED, 0.2f, 0D, 1D));
         try {
             player.setWalkSpeed(level);
         } catch (IllegalArgumentException e) {
             SkillAPI.inst().getLogger().warning("Attempted to set player speed to " + level + " but failed: " + e.getMessage());
         }
-
     }
 
     private double getModifiedMaxHealth(Player player) {
