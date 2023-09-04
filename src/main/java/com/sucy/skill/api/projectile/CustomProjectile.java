0740ea86ddc3ca8411d5796d67e9020d96afb8d5
RobotHanzo <RobotHanzo@users.noreply.github.com>
2023-09-04 08:04:33 +0800

    Stop crashing

--- src/main/java/com/sucy/skill/api/projectile/CustomProjectile.java
+++ src/main/java/com/sucy/skill/api/projectile/CustomProjectile.java
@@ -278,7 +278,7 @@ public abstract class CustomProjectile extends BukkitRunnable implements Metadat
      */
     protected boolean isTraveling() {
         // Leaving a loaded chunk
-        if (!getLocation().getChunk().isLoaded()) {
+        if (!getLocation().isWorldLoaded() || !getLocation().getChunk().isLoaded()) {
             cancel();
             Bukkit.getPluginManager().callEvent(expire());
             return false;
