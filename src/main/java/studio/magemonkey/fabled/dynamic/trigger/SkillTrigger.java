a302e1c3fdc67a745962c343a6b2cbc5591127f0
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2024-05-18 23:47:24 +0800

    Fix on skill damage

--- src/main/java/studio/magemonkey/fabled/dynamic/trigger/SkillTrigger.java
+++ src/main/java/studio/magemonkey/fabled/dynamic/trigger/SkillTrigger.java
@@ -1,5 +1,6 @@
 package studio.magemonkey.fabled.dynamic.trigger;
 
+import studio.magemonkey.fabled.Fabled;
 import studio.magemonkey.fabled.api.Settings;
 import studio.magemonkey.fabled.api.event.SkillDamageEvent;
 import studio.magemonkey.fabled.dynamic.DynamicSkill;
@@ -28,7 +29,8 @@ public abstract class SkillTrigger implements Trigger<SkillDamageEvent> {
         final double       min   = settings.getDouble("dmg-min");
         final double       max   = settings.getDouble("dmg-max");
         final List<String> types = settings.getStringList("category");
-        final boolean      empty = types.isEmpty() || types.get(0).isEmpty();
+        final boolean empty = types.isEmpty() || types.get(0).isEmpty() || types.get(0).equals("default");
+        if (!empty) Fabled.inst().getLogger().warning("Skill damage trigger category is not empty, values: " + types);
         return event.getDamage() >= min && event.getDamage() <= max &&
                 (empty || types.contains(event.getClassification()));
     }
