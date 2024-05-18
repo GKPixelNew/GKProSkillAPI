e24c031f15bff3d2dd4843f6267b42ceee5832ae
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2024-05-18 21:59:25 +0800

    Default non-player entities' attribute values to 0

--- src/main/java/studio/magemonkey/fabled/dynamic/condition/AttributeCondition.java
+++ src/main/java/studio/magemonkey/fabled/dynamic/condition/AttributeCondition.java
@@ -26,10 +26,10 @@
  */
 package studio.magemonkey.fabled.dynamic.condition;
 
-import studio.magemonkey.fabled.Fabled;
-import studio.magemonkey.fabled.api.player.PlayerData;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import studio.magemonkey.fabled.Fabled;
+import studio.magemonkey.fabled.api.player.PlayerData;
 
 public class AttributeCondition extends ConditionComponent {
     private static final String ATTR = "attribute";
@@ -38,14 +38,16 @@ public class AttributeCondition extends ConditionComponent {
 
     @Override
     boolean test(final LivingEntity caster, final int level, final LivingEntity target) {
-        if (!(target instanceof Player)) return false;
-
-        final String attr = settings.getString(ATTR, null);
+        int value = 0;
         final int    min  = (int) parseValues(caster, MIN, level, 0);
         final int    max  = (int) parseValues(caster, MAX, level, 999);
 
-        final PlayerData data  = Fabled.getData((Player) target);
-        final int        value = data.getAttribute(attr);
+        if (target instanceof Player player) {
+            final String attr = settings.getString(ATTR, null);
+
+            final PlayerData data = Fabled.getData(player);
+            value = data.getAttribute(attr);
+        }
         return value >= min && value <= max;
     }
 
