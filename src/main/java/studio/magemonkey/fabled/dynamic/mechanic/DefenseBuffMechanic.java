5c6e2213d47e2922f32d821655a10ab81ef8d372
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2025-05-18 01:13:11 +0800

    Def buff debug

--- src/main/java/studio/magemonkey/fabled/dynamic/mechanic/DefenseBuffMechanic.java
+++ src/main/java/studio/magemonkey/fabled/dynamic/mechanic/DefenseBuffMechanic.java
@@ -33,6 +33,8 @@ import studio.magemonkey.fabled.api.util.BuffType;
 
 import java.util.List;
 
+import static studio.magemonkey.fabled.api.util.BuffManager.getBuffData;
+
 /**
  * Applies a flag to each target
  */
@@ -73,7 +75,7 @@ public class DefenseBuffMechanic extends MechanicComponent {
                     skill ? BuffType.SKILL_DEFENSE : BuffType.DEFENSE,
                     new Buff(this.skill.getName(), value, percent),
                     ticks);
-            System.out.println("DefenseBuffMechanic: " + target.getName() + " " + value + " " + percent);
+            System.out.println("new buff: " + target.getName() + " " + getBuffData(target, true));
         }
         return true;
     }
