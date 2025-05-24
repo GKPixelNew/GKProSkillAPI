eeb440f0851fee382aef67825177d417091185b3
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2025-05-25 02:44:34 +0800

    Remove debug logging from BuffListener
    The unnecessary debug print statement has been removed to clean up the code and prevent unwanted console output. This change improves code readability and avoids potential performance issues caused by excessive logging.

--- src/main/java/studio/magemonkey/fabled/listener/BuffListener.java
+++ src/main/java/studio/magemonkey/fabled/listener/BuffListener.java
@@ -35,7 +35,6 @@ public class BuffListener extends FabledListener {
         if (classification != null && !classification.equalsIgnoreCase(PHYSICAL)) {
             damageType = BuffType.SKILL_DAMAGE.getLocalizedName() + "_" + classification;
             defenseType = BuffType.SKILL_DEFENSE.getLocalizedName() + "_" + classification;
-            System.out.println(damageType + " " + defenseType);
         }
 
         double withDamageBuffs = damage;
