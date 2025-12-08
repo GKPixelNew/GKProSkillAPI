74a4c1a7aaae584aabefaa5293cb5df3be6b5c75
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2025-12-09 05:44:28 +0800

    Consider own wolf as ally as well

--- src/main/java/studio/magemonkey/fabled/data/Settings.java
+++ src/main/java/studio/magemonkey/fabled/data/Settings.java
@@ -1017,8 +1017,7 @@ public class Settings extends com.sucy.skill.data.Settings {
      */
     public boolean isAlly(LivingEntity attacker, LivingEntity target) {
         return !canAttack(attacker, target) || (target instanceof Wolf tWolf && tWolf.isTamed()
-                && !attacker.equals(tWolf.getOwner())
-                && !canAttack(attacker, (Player) tWolf.getOwner()));
+                && (attacker.equals(tWolf.getOwner()) || !canAttack(attacker, (Player) tWolf.getOwner())));
     }
 
     /**
