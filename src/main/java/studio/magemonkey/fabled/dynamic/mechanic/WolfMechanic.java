16f7e636be9eaa093a12186b57c97508a2ca9f71
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2025-12-09 04:27:25 +0800

    Debug wolf

--- src/main/java/studio/magemonkey/fabled/dynamic/mechanic/WolfMechanic.java
+++ src/main/java/studio/magemonkey/fabled/dynamic/mechanic/WolfMechanic.java
@@ -114,7 +114,7 @@ public class WolfMechanic extends MechanicComponent {
         List<LivingEntity> aggroTargets = List.of();
         if (aggroTarget != null) {
             aggroTargets = aggroTarget.getTargets(caster, level, targets);
-            log.info(aggroTargets.toString());
+            log.info(aggroTarget.getSettings().toString());
         }
         for (LivingEntity target : targets) {
             for (int i = 0; i < amount; i++) {
