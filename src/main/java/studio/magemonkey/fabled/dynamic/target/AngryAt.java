212eec3b3c21bf13f191fe73d03d626056954e4f
Choi Yiu Hei <blackrc3636@gmail.com>
2025-12-24 23:43:15 +0800

    Create AngryAt.java

---
+++ src/main/java/studio/magemonkey/fabled/dynamic/target/AngryAt.java
@@ -0,0 +1,31 @@
+package studio.magemonkey.fabled.dynamic.target;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Mob;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Team;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class AngryAt extends TargetComponent {
+    private static final String TYPE = "type";
+
+    @Override
+    public String getKey() {
+        return "angry at";
+    }
+
+    @Override
+    public List<LivingEntity> getTargets(LivingEntity caster, int level, List<LivingEntity> targets) {
+        List<LivingEntity> result = new LinkedList<>();
+        if (caster instanceof Mob) {
+            LivingEntity target = ((Mob) caster).getTarget();
+            if (target != null) {
+                result.add(target);
+            }
+        }
+        return result;
+    }
+}
