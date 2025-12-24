a1c4b2d96c6778de1d5721c365858ad79b28c916
Choi Yiu Hei <blackrc3636@gmail.com>
2025-12-25 00:03:49 +0800

    Removed logs

--- src/main/java/studio/magemonkey/fabled/dynamic/target/AngryAt.java
+++ src/main/java/studio/magemonkey/fabled/dynamic/target/AngryAt.java
@@ -23,12 +23,9 @@ public class AngryAt extends TargetComponent {
         List<LivingEntity> result = new LinkedList<>();
         if (caster instanceof Mob) {
             LivingEntity target = ((Mob) caster).getTarget();
-            Fabled.inst().getLogger().info("[AngryAt] Caster: " + caster.getName() + ", Target: " + (target != null ? target.getName() : "null"));
             if (target != null) {
                 result.add(target);
             }
-        } else {
-            Fabled.inst().getLogger().info("[AngryAt] Caster " + caster.getName() + " is not a Mob, skipping");
         }
         return result;
     }
