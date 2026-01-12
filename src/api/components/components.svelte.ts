d6de6c8d4af65eae79ea993d2bb61f919d8bb7ba
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2026-01-12 23:19:48 +0800

    Add 'Ride' option to projectile settings for caster interaction

--- src/api/components/components.svelte.ts
+++ src/api/components/components.svelte.ts
@@ -2457,6 +2457,8 @@ const projectileOptions = (): ComponentOption[] => {
 			.setTooltip('The number of projectiles to fire'),
 		new BooleanSelect('Target Blocks', 'target-blocks', false)
 			.setTooltip('Whether to execute children if the projectile hits a block'),
+		new BooleanSelect('Ride', 'ride', false)
+			.setTooltip('Whether the caster should ride the projectile'),
 
 		// Cone values
 		new AttributeSelect('Angle', 'angle', 30)
@@ -4220,8 +4222,6 @@ class ParticleProjectileMechanic extends FabledMechanic {
 					.setTooltip('Air resistance of the projectile, in inverse seconds. Greater values mean the projectile will slow down more over time, and reach a lower terminal velocity.'),
 				new IntSelect('Particle period', 'period', 2)
 					.setTooltip('How often to play a particle effect where the projectile is.'),
-				new BooleanSelect('Ride', 'ride', false)
-					.setTooltip('Whether the caster should ride the projectile'),
 				new BooleanSelect('Trigger children multiple times', 'multi-callback', true)
 					.setTooltip('Whether to trigger the child components multiple times for any entities hit with respect to "amount".'),
 				...homingOptions(),
